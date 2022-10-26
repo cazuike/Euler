bb65add60360a63ed3659d47ad3831f2398d3ab2
cazuike <chidi15azuike@gmail.com>
2022-10-26 00:11:36 -0400

    tweak

--- Problem1.py
+++ Problem1.py
@@ -1,5 +1,5 @@
 total = 0
-for i in range(1001): #last number is exclusive so +1 to include 10000
+for i in range(1000): #last number is exclusive so +1 to include 10000
     if i % 3 == 0 or i % 5 ==0 :
         total += i
 print(total)
