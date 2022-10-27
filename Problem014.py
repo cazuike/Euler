4576992c904557d346744e5ecbb498a756d531ef
cazuike <chidi15azuike@gmail.com>
2022-10-27 02:12:59 -0400

    need to sleep

---
+++ Problem014.py
@@ -0,0 +1,16 @@
+#could use a hash table to memorize prior calculations to reduce run time!
+k = 0
+num = None
+for i in range(1,1000000):
+    di = i
+    z = 0
+    while di != 1:
+        if di % 2 == 0:
+            di = di // 2
+        else:
+            di = 3*di+1
+        z += 1
+    if z > k:
+        k = z
+        num = i
+#brute force answer 837799
---
+++ Problem015.py
@@ -0,0 +1,19 @@
+count = [0]
+def dp(x,y): #unoptimizied dynamic programming
+    if x == y == 0:
+        count[0] += 1
+    if x < 0 or y < 0:
+        return
+    dp(x-1,y)
+    dp(x,y-1)
+
+memo = {}
+def optimal(x,y): #uses memoization <- way better solution !!
+    if (x,y) in memo:
+        return memo[(x,y)]
+    if x == y == 0:
+        return 1
+    if x < 0 or y< 0:
+        return 0
+    memo[(x,y)] = optimal(x-1,y) + optimal(x,y-1)
+    return memo[(x,y)]
