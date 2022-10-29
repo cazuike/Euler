ad3a3b05e9feffe4041a8b8b81a96f7d7b7d24d1
cazuike <chidi15azuike@gmail.com>
2022-10-29 18:06:57 -0400

    consider Miller–Rabin primality test

---
+++ Problem058.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+import itertools
+import math
+
+isPrime = dict()
+primes = []
+isPrime[1] = True
+for i in range(2,1000000000): #sieve of eratosthenes
+    if i not in isPrime:
+        isPrime[i] = True
+        primes.append(i)
+        for j in range(2*i,1000000000,i):
+            isPrime[j] = False
+
+primes = 0
+total = 1
+
+
+for i in itertools.count(3,2):
+    if i**2 > 1000000000:
+        print("out of range of primes")
+        print(primes,total,i)
+        break
+    total += 4
+    if isPrime[i**2]:
+        primes += 1
+    if isPrime[i**2 -(i-1)]:
+        primes += 1
+    if isPrime[i**2 -2*(i-1)]:
+        primes += 1
+    if isPrime[i**2 -3*(i-1)]:
+        primes += 1
+    if primes/total < 0.1:
+        print(i)
+        break
