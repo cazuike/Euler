82b1495693ab8369c5793d97156e5f30ce91b895
cazuike <chidi15azuike@gmail.com>
2022-10-30 22:22:14 -0400

    added solution

--- Problem067.py
+++ Problem067.py
@@ -107,4 +107,4 @@ triangle = [  # Mutable
 for i in range(len(triangle)-2,-1,-1):
     for j in range(len(triangle[i])):
         triangle[i][j] += max(triangle[i+1][j],triangle[i+1][j+1])
-print(triangle[0][0])
+print(triangle[0][0]) #7273
